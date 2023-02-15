22032e5a5291392b41b0153661d610c94b296a49
Shanzida-Rifa <rifashanzida@gmail.com>
2023-02-15 21:35:10 +0600

    again

---
+++ practices.js
@@ -0,0 +1,28 @@
+// problem1
+var momGive = 1000;
+var fruitCost = 700;
+var remain = momGive - fruitCost;
+console.log(remain);
+
+// problem2
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var total = mathematics + biology + chemistry + physics + bangla;
+var average = total / 5;
+var average = average.toFixed(2);
+
+console.log(average);
+
+// problem 3
+var firstLine = 'I am going to be';
+var secondLine = 'an awesome web developer.';
+console.log(firstLine + ' ' + secondLine);
+
+// problem 4
+var num = 119;
+var num2 = 5;
+console.log(num % num2);
